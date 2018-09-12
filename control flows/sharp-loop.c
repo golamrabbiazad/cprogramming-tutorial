2e551e01ab99ec475fa991e66c3ea93b6647ca29
rabbidwan <golamrabbiazad@gmail.com>
2018-09-12 20:53:37 +0600

    sharping c skill from head-first c

---
+++ control flows/sharp-loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    /* code */
+    int c = 10;
+    
+    while(c > 0){
+        /* code */
+        puts("I must not write code in class.");
+        c = c - 1;
+    }
+    
+    return 0;
+}
