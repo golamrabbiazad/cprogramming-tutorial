54de9ac1d39d41f37e129e745589f9c47884f832
rabbidwan <golamrabbiazad@gmail.com>
2018-09-12 21:27:41 +0600

    reversed code

--- control flows/sharp-loop.c
+++ control flows/sharp-loop.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
-// int main()
-// {
-//     /* code */
-//     int c = 10;
+int main()
+{
+    /* code */
+    int c = 10;
     
-//     while(c > 0){
-//         /* code */
-//         puts("I must not write code in class.");
-//         c = c - 1;
-//     }
+    while(c > 0){
+        /* code */
+        puts("I must not write code in class.");
+        c = c - 1;
+    }
     
-//     return 0;
-// }
+    return 0;
+}
 
 
-// int main(int argc, char const *argv[])
-// {
-//     /* code */
-//     char ex[20];
-//     puts("Enter your boyfriend's name: ");
-//     scanf("%19s", ex);
-//     printf("Dear %s, \n\n\tYour're history.\n", ex);
-//     return 0;
-// }
+int main(int argc, char const *argv[])
+{
+    /* code */
+    char ex[20];
+    puts("Enter your boyfriend's name: ");
+    scanf("%19s", ex);
+    printf("Dear %s, \n\n\tYour're history.\n", ex);
+    return 0;
+}
 
 
 int main(int argc, char const *argv[])
